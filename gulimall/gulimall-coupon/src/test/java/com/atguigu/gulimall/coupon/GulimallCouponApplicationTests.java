28a39e9c0cdcc73d88b388007dd1d671b5eeae5f
lijixiang06 <jixiangli1017@gmail.com>
2025-01-20 23:03:43 +0800

    20250120

--- gulimall/gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall/gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -1,4 +1,4 @@
-package com.atguigu.gulimall.gulimallcoupon;
+package com.atguigu.gulimall.coupon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
