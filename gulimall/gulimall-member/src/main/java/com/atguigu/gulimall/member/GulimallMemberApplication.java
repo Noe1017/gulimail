924003ff5d2c3aa6c6907fbe566744c685b2dfec
lijixiang06 <jixiangli1017@gmail.com>
2025-01-26 23:15:22 +0800

    20250126

--- gulimall/gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall/gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -2,8 +2,16 @@ package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*
+	1、想要远程调用别的服务
+	1）、引入open
+* */
+@EnableFeignClients(basePackages="com.atguigu.gulimall.member.feign")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
 
 	public static void main(String[] args) {
